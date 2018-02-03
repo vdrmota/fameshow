06d0a2340afb6a44ecbefba2473ac18ea25cdfdd
vdrmota <vojtech_drmota@college.harvard.edu>
2018-02-03 13:43:22 -0500

    fixed vojtadrmota spelling

--- live-ios/Live/Config.swift
+++ live-ios/Live/Config.swift
@@ -12,6 +12,6 @@ struct Config {
     
     static var rtmpPushUrl = "rtmp://159.89.42.193/live/"
     static var rtmpPlayUrl = "rtmp://159.89.42.193/live/"
-    static var serverUrl = "http://vojtadromta.com:3000"//"http://socket-streaming.herokuapp.com"
+    static var serverUrl = "http://vojtadrmota.com:3000" //"http://socket-streaming.herokuapp.com"
     
 }
