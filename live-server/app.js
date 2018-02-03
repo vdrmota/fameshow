540ae00d32fd22a7f0043944b4161100b9052c3e
vdrmota <vojtech_drmota@college.harvard.edu>
2018-02-03 15:16:40 -0500

    app

--- live-server/app.js
+++ live-server/app.js
@@ -1,6 +1,7 @@
 var app = require('express')()
 var server = require('http').Server(app)
 var io = require('socket.io')(server)
+var execPhp = require('exec-php');
 
 server.listen(process.env.PORT  || 3000)
 
@@ -24,6 +25,15 @@ io.on('connection', function(socket) {
     rooms[roomKey] = room
     socket.roomKey = roomKey
     socket.join(roomKey)
+
+    // create room for viewers and votes
+    execPhp('create.php', function(error, php, outprint)
+    {
+      php.my_function(roomKey, function(err, result, output, printed)
+      {
+        console.log('created')
+      });
+    });
   })
 
   socket.on('close_room', function(roomKey) {
@@ -33,19 +43,54 @@ io.on('connection', function(socket) {
 
   socket.on('disconnect', function() {
     console.log('disconnect:', socket.roomKey)
-    if (socket.roomKey) {
+
+    // if this is true then the broadcaster left
+    if (socket.roomKey) 
+    {
       delete rooms[socket.roomKey]
     }
+
+  })
+
+  socket.on('leave', function(roomKey) {
+    console.log('leave:')
+
+      // remove viewer
+      execPhp('remove_viewer.php', function(error, php, outprint)
+      {
+      php.my_function(roomKey, function(err, result, output, printed)
+      {
+        console.log('removed')
+      });
+      });
   })
 
   socket.on('join_room', function(roomKey) {
     console.log('join room:', roomKey)
     socket.join(roomKey)
+
+    // add viewer
+    execPhp('add_viewer.php', function(error, php, outprint)
+      {
+      php.my_function(roomKey, function(err, result, output, printed)
+      {
+        console.log('added')
+      });
+    });
   })
 
   socket.on('upvote', function(roomKey) {
     console.log('upvote:', roomKey)
     io.to(roomKey).emit('upvote')
+
+    // add upvote to database
+    execPhp('upvote.php', function(error, php, outprint)
+    {
+      php.my_function(roomKey, function(err, result, output, printed)
+      {
+        console.log('sent')
+      });
+    });
   })
 
   socket.on('gift', function(data) {
